c7f5f0380ff0686d898c646473aea48b5f1ae35a
Mehul2410 <mehulgawhale18hc@student.mes.ac.in>
2020-11-22 19:24:24 +0530

    start

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,9 +1,15 @@
 import React from "react";
+import "../css/About.css";
 
 const About = () => {
   return (
-    <div>
-      <h1>12345</h1>
+    <div className="about">
+      <div className="about__Left">
+        <h2>HELLO</h2>
+        <h1>I AM MEHUL GAWHALE</h1>
+        <h3>LEARNING FULL STACK DEVELOPER & UI/UX DESIGNER </h3>
+      </div>
+      <div className="about__Right"></div>
     </div>
   );
 };
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,10 +1,37 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "../css/Home.css";
+import About from "./About";
 
 const Home = () => {
   return (
     <div className="home">
-      <h1>12345</h1>
+      <div className="home__start">
+        <h1>Heyy!!!</h1>
+        <h2>
+          A good programmer looks both ways before crossing a one-way street.
+        </h2>
+      </div>
+      <div className="home__front">
+        <img src={process.env.PUBLIC_URL + "/portfolio-bg.jpg"} />
+      </div>
+      <div className="overlay">
+        <div className="home__Bottom">
+          <div className="home__BottomLeft">
+            <h3>Mehul</h3>
+          </div>
+          <div className="home__BottomRight">
+            <Link to="/">Home</Link>
+
+            <Link to="#About">About</Link>
+
+            <Link to="/Contact">Contact</Link>
+          </div>
+        </div>
+        <div className="home__About" id="About">
+          <About />
+        </div>
+      </div>
     </div>
   );
 };
