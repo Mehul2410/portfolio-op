683e974064e21d5244449b3c941d1e3c3f2450e2
Mehul2410 <mehulgawhale18hc@student.mes.ac.in>
2020-11-22 14:04:59 +0530

    start

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import react from "react";
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import About from "./components/pages/About";
+import Contact from "./components/pages/Contact";
+import Home from "./components/pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      <h1>MEhul</h1>
+    <div className="app">
+      <Router>
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/Contact">
+            <Contact />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
