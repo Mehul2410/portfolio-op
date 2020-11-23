33b4afe32fae6af454e6936e01bd6e43e07b32e4
Mehul2410 <mehulgawhale18hc@student.mes.ac.in>
2020-11-23 20:04:52 +0530

    start

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -17,12 +17,21 @@ const About = () => {
 
       <div className="about__Intro">
         <div className="about__IntroLeft">
-          <img src={process.env.PUBLIC_URL + "/work.svg"} />
+          <img src={process.env.PUBLIC_URL + "/programmer.svg"} />
         </div>
         <div className="about__IntroRight">
-          <h2>HELLO</h2>
-          <h1>I AM MEHUL GAWHALE</h1>
-          <h3>LEARNING FULL STACK DEVELOPER & UI/UX DESIGNER </h3>
+          <h1>LET’S INTRODUCE ABOUT MYSELF</h1>
+          <h3>
+            <br></br>
+            <br></br>
+            Greetings of the time! I am Mehul Gawhale,I am Learning Full Stack
+            Developer as well as UI/UX Designer.Apart from what I do for a
+            living, I am a maker, engineer,blogger, social, tech enthusiast,
+            teacher.<br></br>
+            <br></br>Feel free to share your thoughts, I am all ears. I will try
+            to help in every way possible. Let us embrace the fact that we are
+            humans before anything. Life is beautiful, let's not waste it.
+          </h3>
         </div>
       </div>
     </div>
--- src/components/pages/Skills.js
+++ src/components/pages/Skills.js
@@ -9,3 +9,19 @@ const Skills = () => {
 };
 
 export default Skills;
+
+// .app {
+//   background-color: #eee;
+//   overflow-y: scroll; /* Add the ability to scroll */
+// }
+
+// /* Hide scrollbar for Chrome, Safari and Opera */
+// .app::-webkit-scrollbar {
+//   display: none;
+// }
+
+// /* Hide scrollbar for IE, Edge and Firefox */
+// .app {
+//   -ms-overflow-style: none; /* IE and Edge */
+//   scrollbar-width: none; /* Firefox */
+// }
